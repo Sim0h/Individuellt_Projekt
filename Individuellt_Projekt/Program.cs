329dcfcfecf2e825ff3789283306192cb303a6e5
Simon Ståhl <simon.stahl94@gmail.com>
2023-09-21 11:04:35 +0200

    Add project files.

---
+++ Individuellt_Projekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Individuellt_Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
